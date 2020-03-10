830ed43e9b792fb2fca968267a92e7f8607a324d
Emerson Mellado <emersonmellado@gmail.com>
2020-03-09 20:25:49 -0700

    Review of node site

--- index.js
+++ index.js
@@ -20,13 +20,16 @@ var storage = multer.diskStorage({
 const upload = multer({ storage: storage });
 app.use('/', express.static('public'));
 
+//Setting up pug as template engine
+//using the convention to have all views in views folder.
 app.set('view engine', 'pug');
 
+//Declaring a global array called superheroes with three properties
 const superheroes = [
     { id: 1, name: 'SPIDER-MAN', image: 'spiderman.jpg' },
     { id: 2, name: 'CAPTAIN MARVEL', image: 'captainmarvel.jpg' },
-    { id: 3, name: 'HULK', image: 'hulk.jpg' },
-    { id: 4, name: 'THOR', image: 'thor.jpg' },
+    { id: 3, name: 'Hulk', image: 'hulk.jpg' },
+    { id: 4, name: 'Thor', image: 'thor.jpg' },
     { id: 5, name: 'IRON MAN', image: 'ironman.jpg' },
     { id: 6, name: 'DAREDEVIL', image: 'daredevil.jpg' },
     { id: 7, name: 'BLACK WIDOW', image: 'blackwidow.jpg' },
@@ -34,22 +37,34 @@ const superheroes = [
     { id: 9, name: 'WOLVERINE', image: 'wolverine.jpg' },
 ];
 
+superheroes.reverse();
+
 app.get('/', (req, res) => {
-    res.render('index', { superheroes: superheroes });
+    //internal scope of this function
+    const indexVariables = {
+        pageTitle: "First page of our app",
+        superheroes: superheroes
+    }
+    res.render('index', { variables: indexVariables });
 });
 
-app.get('/create', (req, res)=>{
+app.get('/create', (req, res) => {
+    //internal scope of this function
     res.render('create');
 })
 
 app.get('/superheroes/', (req, res) => {
+    //internal scope of this function
     res.render('superhero', { superheroes: superheroes });
 });
 
 app.get('/superheroes/:id', (req, res) => {
+    //internal scope of this function
     const selectedId = req.params.id;
 
     let selectedSuperhero = superheroes.filter(superhero => {
+        console.log("Superhero: ", superhero.id, superhero.id === +selectedId);
+
         return superhero.id === +selectedId;
     });
 
@@ -58,19 +73,20 @@ app.get('/superheroes/:id', (req, res) => {
     res.render('superhero', { superheroe: selectedSuperhero });
 });
 
-app.post('/superheros', upload.single('file'), (req, res) => {
+app.post('/superheroes', upload.single('file'), (req, res) => {
+    //internal scope of this function
     const newId = superheroes[superheroes.length - 1].id + 1;
-    
+
     const newSuperHero = {
-      id: newId, 
-      name: req.body.superhero.toUpperCase(), 
-      image: req.file.filename
+        id: newId,
+        name: req.body.superhero.toUpperCase(),
+        image: req.file.filename
     }
-    
+
     superheroes.push(newSuperHero);
-    
+
     res.redirect('/');
-  });
+});
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
