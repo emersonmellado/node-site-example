239ce3b31384245ea680de97d517bbf99fb16f01
Emerson Mellado <emersonmellado@gmail.com>
2020-03-23 19:13:21 -0700

    Moving mongodb to mongo atlas

--- index.js
+++ index.js
@@ -4,8 +4,7 @@ const fs = require('fs');
 const port = 3000;
 //Mongoose implementation
 const mongoose = require('mongoose');
-//let uri = "mongodb+srv://node-site-example:node-site-example1234@cluster0-1regk.mongodb.net/comics?retryWrites=true&w=majority";
-let uri = "mongodb://localhost:27017/comics";
+let uri = "mongodb://node-site-example:node-site-example1234@cluster0-shard-00-00-1regk.mongodb.net:27017,cluster0-shard-00-01-1regk.mongodb.net:27017,cluster0-shard-00-02-1regk.mongodb.net:27017/comics?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority";
 mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 //Mongoose Model (Work as a Schema)
